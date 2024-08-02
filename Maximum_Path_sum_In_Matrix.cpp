cefd99a9bcfb825176a038e7fc5792c1ae343c52
Prateek Kanaujia <152027702+Prateek307@users.noreply.github.com>
2024-08-02 17:30:08 +0530

    Add files via upload

--- Maximum_Path_sum_In_Matrix.cpp
+++ Maximum_Path_sum_In_Matrix.cpp
@@ -1,34 +1,73 @@
-// link to the problem --->https://www.naukri.com/code360/problems/maximum-path-sum-in-the-matrix_797998?source=youtube&campaign=striver_dp_videos&leftPanelTabValue=SUBMISSION
+// link to the problem ---> https://www.geeksforgeeks.org/problems/path-in-matrix3805/1
+
+//{ Driver Code Starts
+// Initial Template for C++
 
 #include <bits/stdc++.h>
-int solve(int i, int j, vector<vector<int>> &matrix, int n, int m, vector<vector<int>> &dp)
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
 {
-    if (i >= n || j >= m || j < 0) // Adjusted condition for out of bounds check
-        return INT_MIN;
-
-    if (i == n - 1)
-        return matrix[i][j];
-
-    if (dp[i][j] != -1)
-        return dp[i][j];
-
-    int down = solve(i + 1, j, matrix, n, m, dp);
-    int down_right = solve(i + 1, j + 1, matrix, n, m, dp);
-    int down_left = solve(i + 1, j - 1, matrix, n, m, dp);
-
-    return dp[i][j] = matrix[i][j] + max({down, down_right, down_left});
-}
-int getMaxPathSum(vector<vector<int>> &matrix)
-{
-    //  Write your code here.
-    int n = matrix.size();
-    int m = matrix[0].size();
-    int ans = INT_MIN;
-    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-
-    for (int j = 0; j < m; j++) // Fixed loop limit from n to m
+public:
+    int solve(int i, int j, int n, vector<vector<int>> &arr, vector<vector<int>> &dp)
     {
-        ans = max(solve(0, j, matrix, n, m, dp), ans);
+        if (i == n - 1)
+        {
+            if (j >= 0 && j < n)
+                return arr[i][j];
+            return INT_MIN;
+        }
+        if (j < 0 || j > n - 1)
+            return INT_MIN;
+
+        // Return the stored result if already computed
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        // Compute the maximum sum for all three possible moves
+        int path1 = arr[i][j] + solve(i + 1, j, n, arr, dp);
+        int path2 = arr[i][j] + solve(i + 1, j - 1, n, arr, dp);
+        int path3 = arr[i][j] + solve(i + 1, j + 1, n, arr, dp);
+
+        // Store the result and return it
+        return dp[i][j] = max({path1, path2, path3});
     }
-    return ans;
+
+    int maximumPath(int N, vector<vector<int>> Matrix)
+    {
+        vector<vector<int>> dp(N, vector<int>(N, -1));
+        int ans = INT_MIN;
+
+        // Compute the maximum path sum starting from each column in the first row
+        for (int j = 0; j < N; j++)
+        {
+            ans = max(ans, solve(0, j, N, Matrix, dp));
+        }
+
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        vector<vector<int>> Matrix(N, vector<int>(N, 0));
+        for (int i = 0; i < N * N; i++)
+            cin >> Matrix[(i / N)][i % N];
+
+        Solution ob;
+        cout << ob.maximumPath(N, Matrix) << "\n";
+    }
+    return 0;
 }
+// } Driver Code Ends
